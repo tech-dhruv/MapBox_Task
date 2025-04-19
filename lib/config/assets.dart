cceb37d05e685d46f518943f58e16e9c4ef35747
Dhruv <dsenjaliya54@gmail.com>
2025-04-19 11:59:48 +0530

    add assets

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -15,6 +15,30 @@ class Assets {
   static String ONBOARDING_2 = IMAGES + "onboarding_2.png";
   static String ONBOARDING_3 = IMAGES + "onboarding_3.png";
 
+  //map markers assests
+  static String BLUE_PIN = IMAGES + 'blue-marker.png';
+  static String VISIT_PURPLE_PIN = IMAGES + 'visit-purple.svg';
+
+  //customer-store markers assests
+
+  //bronze
+  static String BRONZE_GRAY = IMAGES + 'bronze-gray.svg';
+  static String BRONZE_GREEN = IMAGES + 'bronze-green.svg';
+  static String BRONZE_RED = IMAGES + 'bronze-red.svg';
+  static String BRONZE_YELLOW = IMAGES + 'bronze-yellow.svg';
+
+  //silver
+  static String SILVER_GRAY = IMAGES + 'silver-gray.svg';
+  static String SILVER_GREEN = IMAGES + 'silver-green.svg';
+  static String SILVER_RED = IMAGES + 'silver-red.svg';
+  static String SILVER_YELLOW = IMAGES + 'silver-yellow.svg';
+
+  //gold
+  static String GOLD_GRAY = IMAGES + 'gold-gray.svg';
+  static String GOLD_GREEN = IMAGES + 'gold-green.svg';
+  static String GOLD_RED = IMAGES + 'gold-red.svg';
+  static String GOLD_YELLOW = IMAGES + 'gold-yellow.svg';
+
   // Icons
   static String ICON = ICONS + "app_icon.png";
 }
